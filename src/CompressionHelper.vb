b4b5aaaeb556490afa1acce789ce3ebd2708adc5
Kale <kdmny30@gmail.com>
2010-02-05 16:28:19 -0800

    setting cache headers

--- src/CompressionHelper.vb
+++ src/CompressionHelper.vb
@@ -9,8 +9,40 @@ Namespace com.InspectorMu.Web
         Public Shared Sub GetTextResult(ByRef context As HttpContext, ByVal type As MimeType)
             Dim text = Compress(context, type)
             context.Response.ContentType = MimeTypeHelper.GetString(type)
+            Dim span = DateTime.UtcNow.AddDays(1) - DateTime.UtcNow
+            context.Response.CacheControl = "public"
+            context.Response.Cache.SetLastModified(FindLastModified(context, type))
+            context.Response.Cache.SetMaxAge(span)
+            context.Response.Cache.SetExpires(DateTime.UtcNow.AddDays(1))
             context.Response.Output.Write(text)
         End Sub
+
+        Protected Shared Function FindLastModified(ByRef context As HttpContext, ByVal type As MimeType)
+            Dim lastModified = DateTime.MinValue
+            Dim extension As String
+            Select Case type
+                Case MimeType.TextCss
+                    extension = "css"
+                    Exit Select
+                Case MimeType.ApplicationJavaScript
+                    extension = "js"
+                    Exit Select
+                Case Else
+                    Throw New NotImplementedException()
+            End Select
+            Dim id = context.Request.QueryString("id")
+            Dim names As String() = id.Split(","c)
+            For Each name As String In names
+                Dim path = String.Format("~/{0}.{1}", name, extension)
+                path = context.Server.MapPath(path)
+                If File.Exists(path) Then
+                    If File.GetLastWriteTimeUtc(path) > lastModified Then
+                        lastModified = File.GetLastWriteTimeUtc(path)
+                    End If
+                End If
+            Next
+            Return lastModified
+        End Function
 
         Protected Shared Function Compress(ByRef context As HttpContext, ByVal type As MimeType) As String
             Dim builder As New StringBuilder()
